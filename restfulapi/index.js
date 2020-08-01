13ed9d0ad76fd634a280d906c241e58b4c6fe80f
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-08-01 16:09:59 +0100

    Can send data - currently just name

--- restfulapi/index.js
+++ restfulapi/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const pool = require('./db');
 
 
+app.use(express.json());
 
+app.post('/characters', async (req, res) => {
+    try {
+        console.log(req.body);
+        const { name } = req.body;
+        const character = await pool.query("INSERT INTO characters (name) VALUES($1) RETURNING *", [name]);
+
+        res.json(character.rows[0]);
+    } catch (err) {
+        console.log(err.message);
+    }
+})
 
 
 app.listen(5000, function()  {
