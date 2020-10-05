a7690a4f6d965b2c81df5396204fd758880cbc7e
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-10-05 18:03:19 +0100

    Updates find the sum of a range of numbers

--- kata/js/sum.js
+++ kata/js/sum.js
@@ -1,6 +1,9 @@
 sumAll = (arr)  => {
+    // find the minimum
     let max = Math.max(arr[0], arr[1]);
+    // find the maximum
     let min = Math.min(arr[0], arr[1]);
+    // find the sum of the array using min as the first num in the array and max as the last number in the array
     let sum = 0;
     for (let i = min; i <= max; i++) {
       sum += i;
@@ -9,3 +12,5 @@ sumAll = (arr)  => {
   }
   
   console.log(sumAll([1, 4]));
+  // 1 + 2+ 3+ 4
+  // 10
